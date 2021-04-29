36cf2cbbd28ec987f163a7b599973ec0b2263d9e
swati <swati.srivastava@cyfuture.com>
2021-04-30 01:18:56 +0530

    Minor changes

--- Furniture/views.py
+++ Furniture/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mdfwalldecor(request):
+    context = {
+        'product_name': 'Santosha Decor MDF Metallic Paint Wall Deocration Shelf Rack, (Red and White) - Set of 6',
+        'price': '1,099'
+    }
+    return render(request, 'Furniture/mdfwalldecor.html', context=context)
